742da68ad17f55df7851cc48420b0f16f65d919f
riiski <riiski@users.noreply.github.com>
2025-08-13 08:35:48 +0300

    pajudinom reikaliuką

--- media.php
+++ media.php
@@ -3,7 +3,7 @@ $directory = "img";
 
 $images = array_filter(scandir($directory), function($file) use ($directory) {
     $ext = pathinfo($file, PATHINFO_EXTENSION);
-    return in_array(strtolower($ext), ['jpg', 'jpeg', 'png', 'gif', 'webp', 'mp4']);
+    return in_array(strtolower($ext), ['jpg', 'jpeg', 'png', 'gif', 'webp']);
 });
 echo json_encode(array_values($images));
 ?>
